4052ce582eb85d7842185d8fcec79d032d033482
mikuza32 <mikuza32@gmail.com>
2024-10-21 14:49:38 -0400

    Home and Metrics page logic and formatting finished

---
+++ src/Home/Home.jsx
@@ -0,0 +1,237 @@
+// eslint-disable-next-line no-unused-vars
+import React, {useState, useEffect} from 'react'
+import './Home.css';
+
+
+function Home() {
+    //Storing for list of records, populated with demo data for now until backend is finished
+    const [records, setRecords] = useState([
+        {id: 1, date: '2024-10-19', time: '11:00', machine: 'West Freezer', machineTempF: 10, room: 'Room 205', roomTempF: 69, location: 'West' },
+        {id: 2, date: '2024-10-19', time: '11:30', machine: 'East Freezer', machineTempF: 5, room: 'Room 107', roomTempF: 71, location: 'East' }
+    ]);
+
+
+    //keeps track of all records from users that use crud operations
+    const [selectedRecords, setSelectedRecords] = useState([]);
+    // handles form data objects
+    const [formData, setFormData] = useState({
+        user: '',
+        location: '',
+        room: '',
+        machine: '',
+        machineTempF: '',
+        roomTempF: '',
+        date: '',
+        time: '',
+    });
+
+    // toggles boolean admin view to true for now, until backend is finished to show record deletion
+    const [isAdmin, setIsAdmin] = useState(true); //true to show working webpage
+    const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+
+    // useEffect hook used to perform fetching records
+    useEffect(() => {
+        fetchRecords();
+    }, []);
+
+
+    // placeholder method to function fake records, until backend is implemented
+    const fetchRecords = () => {
+        // not fetching anything until backend is implemented
+    };
+
+
+    // function to handle selecting or deselecting records
+    const handleSelectRecord = (recordId) => {
+        if (selectedRecords.includes(recordId)) {
+            setSelectedRecords(selectedRecords.filter(id => id !== recordId));
+        } else {
+            setSelectedRecords([...selectedRecords, recordId]);
+        }
+    };
+
+
+    // handles user input in the form for updates from user
+    const handleFormChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+
+    //clears the form data and resets the input fields for the user
+    const handleClearForm = () => {
+        setFormData({
+            user: '',
+            location: '',
+            room: '',
+            machine: '',
+            machineTempF: '',
+            roomTempF: '',
+            date: '',
+            time: '',
+        });
+    };
+
+
+    // adds a new record based on user input into the form (temp)
+    const handleSave = () => {
+        const newRecord = {
+            id: records.length + 1,
+            ...formData                      //demo adding a new record for now, until backened is developed and connected
+        };
+        setRecords([...records, newRecord]);
+        handleClearForm();
+    };
+
+
+    // deletes selected records chosen by the user (temp until backend is implemented)
+    const handleDelete = () => {
+        // demo deleting records until backend is developed
+        const remainingRecords = records.filter(record => !selectedRecords.includes(record.id));
+        setRecords(remainingRecords); // updates the form with the records remaining
+        setSelectedRecords([]); // clears the selected records chosen by the user
+        setDeleteDialogOpen(false); // closes the delete confirmation dialog
+    };
+
+    return (
+        <div className="homePageView">
+            <div className="splitLayout">
+                <div className="gridWrapper">
+                    <table className="recordGrid">
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Machine</th>
+                            <th>Machine Temp (℉)</th>
+                            <th>Room</th>
+                            <th>Room Temp (℉)</th>
+                            <th>Location</th>
+                            <th>Username</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {records.map(record => (
+                            <tr key={record.id}
+                                onClick={() => handleSelectRecord(record.id)} // on user click user can select/deselect a record
+                                className={selectedRecords.includes(record.id) ? 'selected' : ''}
+                            >
+                                <td>{record.date}</td>
+                                <td>{record.time}</td>
+                                <td>{record.machine}</td>
+                                <td>{record.machineTempF}</td>
+                                <td>{record.room}</td>
+                                <td>{record.roomTempF}</td>
+                                <td>{record.location}</td>
+                                <td>{record.username}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="editorLayout">
+                    <form className="formLayout">
+                        {/* Each field of the form for the user to input temp data */}
+                        <div>
+                            <label>User</label>
+                            <input
+                                type="text"
+                                name="user"
+                                value={formData.user}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Location</label>
+                            <input
+                                type="text"
+                                name="location"
+                                value={formData.location}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Room</label>
+                            <input
+                                type="text"
+                                name="room"
+                                value={formData.room}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Machine</label>
+                            <input
+                                type="text"
+                                name="machine"
+                                value={formData.machine}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Machine Temp (℉)</label>
+                            <input
+                                type="number"
+                                name="machineTempF"
+                                value={formData.machineTempF}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Room Temp (℉)</label>
+                            <input
+                                type="number"
+                                name="roomTempF"
+                                value={formData.roomTempF}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Date</label>
+                            <input
+                                type="date"
+                                name="date"
+                                value={formData.date}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div>
+                            <label>Time</label>
+                            <input
+                                type="time"
+                                name="time"
+                                value={formData.time}
+                                onChange={handleFormChange}
+                            />
+                        </div>
+                        <div className="buttonLayout">
+                            <button type="button" onClick={handleSave}>Save</button> {/* Saves updated info added*/}
+                            <button type="button" onClick={handleClearForm}>Clear</button> {/*Clears all of the info added into the form */}
+                        </div>
+                    </form>
+                    {isAdmin && ( // only shows the delete button if the user is an admin
+                        <button
+                            className="deleteButton"
+                            onClick={() => setDeleteDialogOpen(true)}
+                        >
+                            Delete Selected Records
+                        </button>
+                    )}
+                </div>
+            </div>
+            {deleteDialogOpen && ( // Delete confirmation printout to the user if they are an admin
+                <div className="confirmDialog">
+                    <p>Are you sure you want to delete these records?</p>
+                    <button onClick={handleDelete}>Confirm</button>
+                    <button onClick={() => setDeleteDialogOpen(false)}>Cancel</button>
+                </div>
+            )}
+        </div>
+
+    );
+}
+
+export default Home;
---
+++ src/Metrics/Metrics.jsx
@@ -0,0 +1,182 @@
+// src/Metrics.jsx
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import './Metrics.css';
+import { PolarArea, Bar } from 'react-chartjs-2';
+// usage of importing Chart.js library components for chart functionality on the metrics page
+import {
+    Chart as ChartJS,
+    RadialLinearScale,
+    LinearScale,
+    CategoryScale,
+    ArcElement,
+    BarElement,
+    Tooltip,
+    Legend
+} from 'chart.js';
+// registers the Chart.js components that will be used in the charts due to be created from the data retrieved
+ChartJS.register(
+    RadialLinearScale,
+    LinearScale,
+    CategoryScale,
+    ArcElement,
+    BarElement,
+    Tooltip,
+    Legend
+);
+
+function Metrics() {
+    //tracks the active state of the menu
+    const [menuActive, setMenuActive] = useState(false);
+
+    // toggles the menu's active state
+    const toggleMenu = () => {
+        setMenuActive(!menuActive);
+    };
+
+    // fake data for the polar area chart (temporary until backend is implemented to make calls and display real data real time)
+    const polarData = {
+        labels: ['West Campus', 'East Campus', 'Osceola Campus'],
+        datasets: [
+            {
+                label: 'Weekly Average',
+                data: [35, 37, 38],                           // all fake data to demo, eventually will be refactored to make backend calls and be manipulated
+                backgroundColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                ],
+            },
+        ],
+    };
+
+
+    // fake data for the barchart (temporary until backend is implemented to make calls and display real data real time)
+    const barData = {
+        labels: ['West Campus', 'East Campus', 'Osceola Campus'],
+        datasets: [
+            {
+                label: 'Temperature',
+                data: [35, 37, 38],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 99, 132, 1)',                // all fake data to demo, eventually will be refactored to make backend calls and be manipulated
+                    'rgba(255, 206, 86, 1)',
+                ],
+            },
+        ],
+    };
+
+    // maintains aspect ratio and makes the charts responsive (hovering, load animation etc)
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+    };
+
+    return (
+        <div className="container">
+            <div className="main">
+                {/* Toggle menu, search bar for variouis metrics, and VCentials logo image for intuitiveness */}
+                <div className="topbar">
+                    <div className="toggle" onClick={toggleMenu}>
+                        <ion-icon name="menu-outline"></ion-icon>
+                    </div>
+                    <div className="search">
+                        <label>
+                            <input type="text" placeholder="Search here" />
+                            <ion-icon name="search-outline"></ion-icon>
+                        </label>
+                    </div>
+                    <div className="user">
+                        <img src="VCentials.jpg" alt="User" />
+                    </div>
+                </div>
+
+                {/* Card box section to display statistics for VCentials */}
+                <div className="cardBox">
+                    {[
+                        { number: 56, name: "Weekly Reports" },
+                        { number: 125, name: "Monthly Reports" },
+                        { number: 276, name: "Quarterly Reports" },       // fake data for weekly, monthly, quarterly, and annual reports for VCentials (once backend is implemented it will be refactored)
+                        { number: 531, name: "Annual Reports" },
+                    ].map((card, index) => (
+                        <div className="card" key={index}>
+                            <div>
+                                <div className="numbers">{card.number}</div>
+                                <div className="cardName">{card.name}</div>
+                            </div>
+                            <div className="iconBx">
+                                <ion-icon name="newspaper-outline"></ion-icon>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                {/* Charts section to display the polar chart and bar chart of temp data */}
+                <div className="chartsBx">
+                    <div className="chart">
+                        <PolarArea data={polarData} options={chartOptions} />
+                    </div>
+                    <div className="chart">
+                        <Bar data={barData} options={chartOptions} />
+                    </div>
+                </div>
+
+                {/* Details Section */}
+                <div className="details">
+                    <div className="recentOrders">
+                        <div className="cardHeader">
+                            <h2>Campus Updates</h2>
+                            <a href="#" className="btn">View All</a>
+                        </div>
+                        <table>
+                            <thead>
+                            <tr>
+                                <td>Location</td>
+                                <td>Temperature</td>
+                                <td>Category</td>
+                                <td>Status</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {[
+                                { location: 'West Campus', temp: 35, category: 'Cool', status: 'Good' },
+                                { location: 'East Campus', temp: 37, category: 'Cool', status: 'Good' },
+                                { location: 'Osceola Campus', temp: 38, category: 'Cool', status: 'Good' },   // fake data of recent orders and their respective status
+                            ].map((order, index) => (
+                                <tr key={index}>
+                                    <td>{order.location}</td>
+                                    <td>{order.temp}</td>                       {/* Print out of data in this order */}
+                                    <td>{order.category}</td>
+                                    <td><span className="status good">{order.status}</span></td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {/* Location reports section that displays each report details for each campus */}
+                    <div className="locationReports">
+                        <div className="cardHeader">
+                            <h2>Location Reports</h2>
+                        </div>
+                        <table>
+                            {['West Campus', 'East Campus', 'Osceola Campus'].map((location, index) => (
+                                <tr key={index}>
+                                    <td width="60px">
+                                        <div className="imgBx"><img src="/VCentials.jpg" alt="Location" /></div>
+                                    </td>
+                                    <td>
+                                        <h4>{location} <br /> <span>Campus</span></h4>
+                                    </td>
+                                </tr>
+                            ))}
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Metrics;
