e9f6a6597f3fd05f262127d5513ddecdff8e2fc6
vcentials <vcentials@gmail.com>
2024-11-30 12:54:18 -0500

    Firestore Initiate

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const API_KEY =  import.meta.env.VITE_API_KEY || process.env.VITE_API_KEY;
+export const AUTH_DOMAIN =
+  import.meta.env.VITE_AUTH_DOMAIN || process.env.VITE_AUTH_DOMAIN;
+export const PROJECT_ID =
+  import.meta.env.VITE_PROJECT_ID || process.env.VITE_PROJECT_ID;
+export const STORAGE_BUCKET =
+  import.meta.env.VITE_STORAGE_BUCKET || process.env.VITE_STORAGE_BUCKET;
+export const MESSAGING_SENDER_ID =
+  import.meta.env.VITE_MESSAGING_SENDER_ID ||
+  process.env.VITE_MESSAGING_SENDER_ID;
+export const APP_ID = import.meta.env.VITE_APP_ID || process.env.VITE_APP_ID;
+export const MEASUREMENT_ID =
+  import.meta.env.VITE_MEASUREMENT_ID || process.env.VITE_MEASUREMENT_ID;
---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "./constants";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+console.log("🚀 ~ firebaseConfig:", firebaseConfig)
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+import { getAuth } from "firebase/auth"
+export const auth = getAuth(app);
